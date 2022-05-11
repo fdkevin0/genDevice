13485863bb3c81e58df1147f6486cbe3490eb43a
FDKevin <fdkevinzero@gmail.com>
2022-05-11 10:03:25 +0800

    use mirai go template

--- go.mod
+++ go.mod
@@ -3,11 +3,22 @@ module github.com/fdkevin0/gendevice
 go 1.18
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/samber/lo v1.20.0 // indirect
+	github.com/Mrs4s/MiraiGo v0.0.0-20220428155502-a4cf95f79352
+	github.com/google/uuid v1.3.0
+	github.com/samber/lo v1.20.0
+	github.com/tidwall/sjson v1.2.4
+)
+
+require (
+	github.com/RomiChan/protobuf v0.0.0-20220318113238-d8a99598f896 // indirect
+	github.com/RomiChan/syncx v0.0.0-20220320130821-c88644afda9c // indirect
+	github.com/fumiama/imgsz v0.0.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.11 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tidwall/gjson v1.14.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	github.com/tidwall/sjson v1.2.4 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
--- main.go
+++ main.go
@@ -6,47 +6,46 @@ import (
 	"math/rand"
 	"net"
 	"os/exec"
-	"strconv"
 	"strings"
-	"time"
 
+	"github.com/Mrs4s/MiraiGo/client"
 	"github.com/google/uuid"
 	"github.com/samber/lo"
 	"github.com/tidwall/sjson"
 )
 
 var (
-	generatedImei    = GenerateImei()
+	generatedImei    = client.GenIMEI()
 	generatedImeiMD5 = fmt.Sprintf("%x", md5.Sum([]byte(generatedImei)))
 	valuesMap        = map[string]any{
-		"deviceInfoVersion":        2,
-		"data.display":             "adb shell getprop ro.build.id",
-		"data.product":             "adb shell getprop ro.build.product",
-		"data.device":              "adb shell getprop ro.vendor.product.device",
-		"data.board":               "adb shell getprop ro.product.board",
-		"data.brand":               "adb shell getprop ro.product.brand",
-		"data.model":               "adb shell getprop ro.product.model",
-		"data.bootloader":          "unknown",
-		"data.fingerprint":         "adb shell getprop ro.vendor.build.fingerprint",
-		"data.bootId":              strings.ToUpper(uuid.New().String()),
-		"data.procVersion":         "adb shell getprop cat /proc/version",
-		"data.baseBand":            "adb shell getprop ro.baseband",
-		"data.version.incremental": "adb shell getprop ro.product.build.version.incremental",
-		"data.version.release":     "adb shell getprop ro.product.build.version.release",
-		"data.version.codename":    "adb shell getprop ro.build.version.codename",
-		"data.simInfo":             "T-Mobile",
-		"data.osType":              "android",
-		"data.macAddress":          GenerateMac().String(),
-		"data.wifiBSSID":           GenerateMac().String(),
-		"data.wifiSSID":            "<unknown ssid>",
-		"data.imsiMd5":             generatedImeiMD5,
-		"data.imei":                generatedImei,
-		"data.apn":                 "wifi",
+		"display":    "adb shell getprop ro.build.id",
+		"product":    "adb shell getprop ro.build.product",
+		"device":     "adb shell getprop ro.vendor.product.device",
+		"board":      "adb shell getprop ro.product.board",
+		"brand":      "adb shell getprop ro.product.brand",
+		"model":      "adb shell getprop ro.product.model",
+		"bootloader": "unknown",
+		// "finger_print":        "adb shell getprop ro.vendor.build.fingerprint",
+		"boot_id": strings.ToUpper(uuid.New().String()),
+		// "procVersion":         "adb shell getprop cat /proc/version",
+		"base_band":           "adb shell getprop ro.baseband",
+		"version.incremental": "adb shell getprop ro.product.build.version.incremental",
+		"version.release":     "adb shell getprop ro.product.build.version.release",
+		"version.codename":    "adb shell getprop ro.build.version.codename",
+		"sim_info":            "T-Mobile",
+		"os_type":             "android",
+		"macAddress":          GenerateMac().String(),
+		"wifi_bssid":          GenerateMac().String(),
+		"wifi_ssid":           "<unknown ssid>",
+		"imsi_md5":            generatedImeiMD5,
+		"imei":                generatedImei,
+		"apn":                 "wifi",
 	}
 )
 
 func main() {
-	result := "{}"
+	client.GenRandomDevice()
+	result := string(client.SystemDeviceInfo.ToJson())
 	for path, value := range valuesMap {
 		if strings.HasPrefix(fmt.Sprint(value), "adb") {
 			cmd := exec.Command("sh", "-c", fmt.Sprint(value))
@@ -71,34 +70,4 @@ func GenerateMac() net.HardwareAddr {
 	mac = append(mac, buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
 
 	return mac
-}
-
-const IMEI_BASE_DIGITS_COUNT int = 14 // The number of digits without the last - the control one.
-
-func GenerateImei() string {
-	var sum int = 0
-	var toAdd int = 0
-	var digits [IMEI_BASE_DIGITS_COUNT + 1]int
-
-	lolrndsrc := rand.NewSource(time.Now().UnixNano())
-	lolrnd := rand.New(lolrndsrc)
-
-	for i := 0; i < IMEI_BASE_DIGITS_COUNT; i++ {
-		digits[i] = lolrnd.Intn(10)
-		toAdd = digits[i]
-		if (i+1)%2 == 0 {
-			toAdd *= 2
-			if toAdd >= 10 {
-				toAdd = (toAdd % 10) + 1
-			}
-		}
-		sum += toAdd
-	}
-	var ctrlDigit int = (sum * 9) % 10
-	digits[IMEI_BASE_DIGITS_COUNT] = ctrlDigit
-	imei := 0
-	for _, v := range digits {
-		imei = imei*10 + v
-	}
-	return strconv.Itoa(imei)
 }
